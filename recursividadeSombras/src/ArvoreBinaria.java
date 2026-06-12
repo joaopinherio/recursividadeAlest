39b9f1ab91601679721075ab44692357ad5098be
João Pinheiro <143855804+joaopinherio@users.noreply.github.com>
2026-06-12 01:07:07 +0000

    demo arvore1

--- recursividadeSombras/src/ArvoreBinaria.java
+++ recursividadeSombras/src/ArvoreBinaria.java
@@ -9,7 +9,6 @@ public class ArvoreBinaria<T> {
         raiz = new No<>(valor);
     }
 
-    
 
 
     public void imprimir(No<T> atual){
@@ -22,6 +21,32 @@ public class ArvoreBinaria<T> {
 
         imprimir(atual.direita);
 
+    }
+
+    //1
+    public int contarNodos(No<T> atual){
+        int result =1;
+        if(atual != null){
+            if(percorreLista(atual) != null){
+                return result += contarNodos(percorreLista(atual));
+            }
+        }
+        return 0;
+    }
+
+    public No<T> percorreLista(No atual){
+        if(atual == null){
+            return null;
+        }
+
+        if(atual.esquerda != null){
+            return atual.esquerda;
+        }
+        if(atual.direita != null){
+            return atual.direita;
+        }
+
+        return null;
     }
 
 
